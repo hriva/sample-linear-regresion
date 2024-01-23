ae547f9e0ef2b6370ceb2e83ce1ce4a2cbb6f795
hector.riv <books_thief@outlook.com>
2024-01-23 15:21:32 -0600

    fix: send data as arrays instead of series to avoid scikit errors

--- src/main.py
+++ src/main.py
@@ -2,17 +2,19 @@
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # Ingest
 DATA =  "src/assets/BTC-USD.csv"
 DATA1 = 'src/assets/ETH-USD.csv'
-df1 = pd.read_csv(DATA)#, index_col= 'Date', parse_dates = True)
-df2 = pd.read_csv(DATA1)#, index_col= 'Date', parse_dates = True)
+df1 = pd.read_csv(DATA, index_col= 'Date', parse_dates = True)\
+    .sort_index(ascending=True)
+df2 = pd.read_csv(DATA1, index_col= 'Date', parse_dates = True)\
+    .sort_index(ascending=True)
 
 ## Get necesary predictive values only.
-dfx = df2['Adj Close'].sort_index(ascending=False)
-dfy = df1["Adj Close"].sort_index(ascending=False)
+dfx = df2['Adj Close']
+dfy = df1["Adj Close"]
 dfx.corr(dfy)
 
 ## Split
@@ -31,30 +33,26 @@ y_train, y_test = dfy.iloc[:y_train_size], dfy.iloc[y_test_size:]
 
 # Regression
 regressor = LinearRegression()
-regressor.fit(X=x_train,y=y_train)
-#using fit works like a statement
-    #FIT this OBJECT to THESE PARAMETERS 
-#SOME METHODS TRANSFORM OBJECTS 
-#SOME USE THEM AS INPUT
+# Use Values to avoid sending pandas.core.series.Series instead of DataFrame
+regressor.fit(X=x_train.values, y=y_train.values)  
 y_pred = regressor.predict(x_test)
-LinearRegression.fit()
-
+print(y_pred)
 
 
 # Training Sets
 plt.scatter(x_train,y_train, color = 'red')
 plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-plt.title('Salaries in function of Exp ')
-plt.xlabel('Experience in years')
-plt.ylabel('Salary')
+plt.title('Bitcoin vs Ethereum')
+plt.xlabel('Ethereum Closing Price')
+plt.ylabel('Bitcoin Closing Price')
 plt.show()
 
 
 # Test set
 plt.scatter(x_test,y_test, color = 'red')
 plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-plt.title('Salaries in function of Exp ')
-plt.xlabel('Experience in years')
-plt.ylabel('Salary')
+plt.title('Bitcoin vs Ethereum')
+plt.xlabel('Ethereum Closing Price')
+plt.ylabel('Bitcoin Closing Price')
 plt.show()
 
